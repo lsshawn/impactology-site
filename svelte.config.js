95ecfd2621a6bb698ff0f8e1bf0d89e7b80c4aa3
sshawn <github.mlxup@simplelogin.com>
2026-02-22 17:05:23 +0800

    svelte adapter auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,22 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.md'],
-	preprocess: [mdsvex({ extensions: ['.md'] })],
+	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.md'] })],
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			handleMissingId: 'ignore',
+			handleHttpError: ({ status, path }) => {
+				if (path === '/blog' && status === 500) {
+					return;
+				}
+				throw new Error(`${status} ${path}`);
+			}
+		}
 	},
 	vitePlugin: {
 		inspector: {
